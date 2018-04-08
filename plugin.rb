f23a83586ad43fcac76c557a1b23ab2a4ee5551d
Dmitry Fedyuk <dfediuk@gmail.com>
2018-04-08 22:48:15 +0600

    1.4.9

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: df-ask
 # about: «Ask me a question» plugin
-# version: 1.4.8
+# version: 1.4.9
 # authors: Dmitry Fedyuk
 # url: https://github.com/discourse-pro/df-ask
 register_asset 'stylesheets/main.scss'
